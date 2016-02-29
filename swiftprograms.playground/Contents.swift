63b0a2822c5023286f44b1a8ae7855ca41d02a83
naveedgit <mdnaveedurrahman@gmail.com>
2016-02-29 09:34:38 +0530

    replace for loop with for each loop and added rational and point functions

--- swiftprograms.playground/Contents.swift
+++ swiftprograms.playground/Contents.swift
@@ -1,17 +1,13 @@
 //function to find the minimum and maximum number in a given array
 
-func minMax(arr:[Int])->(Int,Int)
-{
+func minMax(arr:[Int])->(Int,Int) {
     assert(arr != [])
     var (min,max)=(arr[0],arr[0])
-    for num in arr{
-        if min > num
-        {
+    for num in arr {
+        if min > num {
             min=num
-            continue
         }
-        if max<num
-        {
+        else if max<num {
             max=num
         }
     }
@@ -42,24 +38,17 @@ assert(minmax.1==8)
 
 
 //function to find weather the given number is prime or not
-func isPrime(num:Int) ->Bool
-{
-    let n=abs(num)
-    if n>1
-    {
-        for var i=2;i<n;i++
-        {
-            if n%i==0
-            {
+func isPrime(num:Int) ->Bool{
+   assert(num>=1)
+    if num<=1{
+        return false
+    }
+ 
+        for i in 2..<num {
+            if num%i==0 {
                 return false
             }
         }
-    }
-    else
-    {
-        return false
-    }
-    
     return true
     
 }
@@ -67,9 +56,7 @@ func isPrime(num:Int) ->Bool
 assert(isPrime(1)==false)
 assert(isPrime(7)==true)
 assert(isPrime(10)==false)
-assert(isPrime(0)==false)
-assert(isPrime(-7)==true)
-assert(isPrime(-9)==false)
+
 
 
 
@@ -77,19 +64,15 @@ assert(isPrime(-9)==false)
 //function to find gcd of given two numbers
 func gcd(x:Int,_ y:Int)->Int
 {
-   if x==0
-   {
+   if x==0 {
     return abs(y)
    }
-   if y==0
-   {
+   if y==0 {
     return abs(x)
    }
     
-    for var i=abs(abs(x)<abs(y) ? x:y); i>=1; i--
-     {
-        if x%i==0 && y%i==0
-        {
+    for var i=abs(abs(x)<abs(y) ? x:y); i>=1; i-- {
+        if x%i==0 && y%i==0 {
             return i;
         }
      }
@@ -109,13 +92,10 @@ assert(gcd(-9,0)==9)
 
 //function to find index of an element
 
-func indexOf(arr:[Int],_ num:Int)->Int?
-{
+func indexOf(arr:[Int],_ num:Int)->Int? {
     assert(arr != [])
-    for n in 0..<arr.count
-    {
-        if arr[n]==num
-        {
+    for n in 0..<arr.count {
+        if arr[n]==num {
             return n
         }
     }
@@ -126,21 +106,20 @@ assert(indexOf([1,2,3,4,5,6],1)==0)
 assert(indexOf([1,2,3,4,5,6],6)==5)
 assert(indexOf([1,2,3,4,5,6],3)==2)
 assert(indexOf([1,7,6,4],0)==nil)
-
+//indexOf([],9)
 
 
 
 //function to remove duplicates in a given sorted array
 func removeDuplicates(arr:[Int]) ->[Int] {
     var unique=[Int]()
-    if arr != []{
+    if arr != [] {
         unique.append(arr[0])
-    }
-    for var i=1;i<arr.count;i++ {
-        if arr[i] != arr[i-1]{
+     for i in 1..<arr.count {
+        if arr[i] != arr[i-1] {
             unique.append(arr[i])
         }
-        
+     }
     }
     return unique
     
@@ -153,12 +132,11 @@ assert(removeDuplicates([1,2,2,3,3,4,4,4,5,6,6,6])==[1,2,3,4,5,6])
 
 
 //function to find reverse of a given array
-func reverseArray(var arr:[Int])->[Int]{
-    let n=arr.count
-    for var i=0; i<n/2; i++ {
+func reverseArray(var arr:[Int])->[Int] {
+    for i in 0..<arr.count/2 {
         let temp=arr[i]
-        arr[i]=arr[n-i-1]
-        arr[n-i-1]=temp
+        arr[i]=arr[arr.count-i-1]
+        arr[arr.count-i-1]=temp
     }
     return arr
 }
@@ -168,3 +146,81 @@ assert(reverseArray([])==[])
 assert(reverseArray([1])==[1])
 assert(reverseArray([1,2])==[2,1])
 assert(reverseArray([1,2,3])==[3,2,1])
+
+
+
+//creating a Point structure with moveBy and distanceTo method
+
+import Foundation
+struct Point {
+    var x=0.0, y=0.0
+    mutating  func moveBy(anotherPoint:Point)->Point {
+        return Point(x:anotherPoint.x,y:anotherPoint.y)
+    }
+    mutating func  distanceTo(anotherPoint:Point)->Double{
+        return sqrt(pow((anotherPoint.x-somePoint.x),2) + pow((anotherPoint.y-somePoint.y),2))
+        
+    }
+}
+var somePoint = Point(x:1.0,y:1.0)
+var anotherPoint=Point(x:3,y:4)
+assert(somePoint.distanceTo(anotherPoint)==3.605551275463989)
+somePoint.moveBy(anotherPoint)
+
+
+
+//creating Rational structure with add, subtract, multiply, divide and compareTo methods in  it
+
+struct Rational{
+    var numerator = 0,denominator = 0
+    init(){
+        self.numerator = 0
+    }
+    init(numerator:Int){
+        self.numerator = numerator
+        self.denominator = 1
+    }
+    init(var numerator:Int,var denominator:Int) {
+        let divisor=gcd(numerator,denominator)
+        numerator /= divisor
+        denominator /= divisor
+        self.numerator=numerator
+        self.denominator=denominator
+    }
+    //addition function
+    func add(num2:Rational)->Rational{
+        return Rational(numerator : (num1.numerator * num2.denominator) + (num2.numerator * num1.denominator) , denominator : (num1.denominator * num2.denominator) )
+    }
+    //subtract function
+    func subtract(num2:Rational)->Rational{
+        return Rational(numerator:(num1.numerator * num2.denominator) - (num2.numerator * num1.denominator) , denominator:(num1.denominator * num2.denominator))
+    }
+    //division function
+    func division(num2:Rational)->Rational{
+        return Rational(numerator: (num1.numerator*num2.denominator) , denominator:(num1.denominator*num2.numerator) )
+    }
+    //multiplication function
+    func multiply(num2:Rational)->Rational{
+        return Rational(numerator:(num1.numerator*num2.numerator) , denominator:(num1.denominator*num2.denominator))
+    }
+    //comparing function
+    func compareTo(num2:Rational)->Int{
+        
+        if num1.numerator/num1.denominator > num2.numerator/num2.denominator {
+            return 1
+        }
+        if num1.numerator/num1.denominator < num2.numerator/num2.denominator{
+            return -1
+        }
+        return 0
+        
+    }
+}
+
+var num1=Rational(numerator:-9,denominator:5)
+var num2=Rational(numerator:2,denominator:5)
+print(num1.add(num2))
+print(num1.subtract(num2)) //but with this instruction a error is arrasing named with stack over flow or under flow)
+print(num1.division(num2))
+print(num1.multiply(num2))
+print(num1.compareTo(num2))
